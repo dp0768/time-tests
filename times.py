f3e1f02889b6e948147c12963ff35d0d4ef50254
Lars Henden <Henden.lars@gmail.com>
2024-10-24 17:37:45 +0100

    Added unit test for test_times.py
    Utilized pytest module to create a unit test for the test_times.py

--- times.py
+++ times.py
@@ -2,12 +2,18 @@ import datetime
 
 
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
+    #Creates dateime object from input strings
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+    
+    #Determines the number of Seconds between the end and start times
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
+    
+    
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
                   start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
-                 for i in range(number_of_intervals)]
+                  for i in range(number_of_intervals)]
+
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
 
