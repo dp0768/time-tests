f3f2b96160a9ae7ed4d6441021598a86b34cb731
dp0768 <ucapend@ucl.ac.uk>
2024-10-29 13:30:25 +0000

    NOT FINISHED - Saving progress of yml data separation issue 17 in case of need to revert

--- test_times.py
+++ test_times.py
@@ -1,5 +1,6 @@
 import times
 import pytest
+import yaml
 
 def test_given_input():
     #standard input time ranges
@@ -94,3 +95,52 @@ def test_backward_interval():
 def test_parameterize_overlap(first_range,second_range,expected_overlap):
     result = times.compute_overlap_time(first_range, second_range)
     assert result == expected_overlap, (f'\nExpected result:{expected_overlap} \n \n Actual result:{result}\nASSERTION MESSAGE END')
+
+
+############################################################################################
+
+#Pytest Parametrization - Separation of code
+
+############################################################################################
+
+def data_load(file_name):
+    with open(file_name, "r")as data_file:
+        return yaml.safe_load(data_file)
+
+
+data = data_load(file_name="fixtures.yml")
+
+#print(type(data))
+
+#for key in data.keys():
+    #print(key)
+
+
+
+@pytest.mark.parametrize("case",dict(data))
+def test_overlap_yml(case):
+    print(type(case))
+    print(case)
+    for KEY in case.keys():
+        #time range 1
+        start_time_1 = case[KEY]['time_range_1']['start_time']
+        end_time_1 = case[KEY]['time_range_1']['end_time']
+        intervals_1 = case[KEY]['time_range_1']['number_of_intervals']
+        interval_gap_1 = case[KEY]['time_range_1']['gap_between_intervals']
+       
+        #time range 2
+        start_time_2 = case[KEY]['time_range_2']['start_time']
+        end_time_2 = case[KEY]['time_range_2']['end_time']
+        intervals_2 = case[KEY]['time_range_2']['number_of_intervals']
+        interval_gap_2 = case[KEY]['time_range_2']['gap_between_intervals']
+       
+        #expected results
+        expected_result = list(case[KEY]['expected'])
+       
+        #Compute overlap and assert result
+        result = times.compute_overlap_time(
+            times.time_range(start_time_1,end_time_1,intervals_1,interval_gap_1),
+            times.time_range(start_time_2,end_time_2,intervals_2,interval_gap_2)
+            )
+        assert result == expected_result, (f'\nExpected result:{expected_result} \n \n Actual result:{result}\nASSERTION MESSAGE END')
+
